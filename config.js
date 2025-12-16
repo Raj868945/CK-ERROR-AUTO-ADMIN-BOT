2f303d043db9f8377e08a4d3aa7777d5770bd552
Raj868945 <rcodex0@gmail.com>
2025-12-16 08:29:09 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+export const BOT_NAME = "CK-ERROR AUTO ADMIN";
+export const OWNER_NAME = "CK-ERROR";
+export const PREFIX = ".";
+export const CHANNEL_NAME = "🖤 BLENDED VIBES 💋";
+export const CHANNEL_LINK = "https://whatsapp.com/channel/0029VbBQQ6v4Y9lenR8ROD3O";
+export const WARN_LIMIT = 3;
